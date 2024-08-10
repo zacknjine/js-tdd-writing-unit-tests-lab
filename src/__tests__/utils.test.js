7e8ea5cedc78d22f03cb8b0ba00373e4996644b5
Zack Njine Wachira <zackwachira45@gmail.com>
2024-08-10 15:58:38 +0300

    Initial commit

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,36 @@
 // Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+  it("returns true when the word is the same forward and backward", () => {
+    const word = "racecar";
+
+    const result = isPalindrome(word);
+
+    expect(result).toBe(true);
+  });
+  it("returns false when the word is not the same forward and backward", () => {
+    const word = "car";
+
+    const result = isPalindrome(word);
+
+    expect(result).toBe(false);
+  });
+  it("return true for words that are a combination of uppercase and lowercase letters", () => {
+    const word = "RaCecar";
+
+    const result = isPalindrome(word);
+
+    expect(result).toBe(true);
+  });
+  it("throws an error if the input has any non-alphabetic characters", () => {
+    const word = "race car";
+
+    expect(() => isPalindrome(word)).toThrow();
+  });
+  it("throws an error if the input is an empty string", () => {
+    const word = "";
+
+    expect(() => isPalindrome(word)).toThrow();
+  });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,12 @@
 // Your code here
+function reverse(string) {
+    return string.split("").reverse().join("");
+  }
+  
+  export function isPalindrome(word) {
+    if (!/^[A-Za-z]+$/.test(word)) {
+      throw new Error("Invalid input");
+    }
+    const lowerCaseWord = word.toLowerCase();
+    return lowerCaseWord === reverse(lowerCaseWord);
+  }
